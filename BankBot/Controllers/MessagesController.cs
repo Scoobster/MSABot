2d0ba496d6f0268577d4e0443e00bbe025147742
TOMDESKTOP\Andrew <andrew-lyall@hotmail.com>
2016-11-24 00:06:39 +1300

    Got LUIS working correctly

--- BankBot/Controllers/MessagesController.cs
+++ BankBot/Controllers/MessagesController.cs
@@ -30,13 +30,64 @@ namespace BankBot
 
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
 
+                StateClient stateClient = activity.GetStateClient();
+                BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id);
+
                 LUISObject.RootObject luisObj;
 
                 HttpClient client = new HttpClient();
                 string x = await client.GetStringAsync(new Uri("https://api.projectoxford.ai/luis/v2.0/apps/a4ab66bf-68bb-4e95-8951-8b179e6f3c21?subscription-key=14b2e4303e5948578989704f3ef1fd87&q=" + activity.Text + "&verbose=true"));
                 luisObj = JsonConvert.DeserializeObject<LUISObject.RootObject>(x);
 
-                Activity reply = activity.CreateReply($"" + luisObj.topScoringIntent.intent);
+                string intent = luisObj.topScoringIntent.intent;
+
+                if (!userData.GetProperty<bool>("SendGreeting")) {
+                    userData.SetProperty<bool>("SendGreeting", true);
+                    await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+                    if (intent == "Greeting" || intent == "None") {
+                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Hello! How can I help you?"));
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    } else {
+                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Hello!"));
+                    }
+                } else {
+                    if (intent == "Greeting") {
+                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Hello Again!"));
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+
+                string output = "";
+
+                if (intent == "None") {
+
+                    await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Sorry I didn't quite understand what you meant."));
+                    output = "Could you please phrase it differently?";
+
+                } else if (intent == "FindBank") {
+
+                    //Make use of GPS and GooglePlacesAPI to give distance to closest bank
+                    //Use Cards
+                    output = "You wish to find a bank...";
+
+                } else if (intent == "CheckBalance") {
+
+                    //Use the database to return amount of money in said bank account
+                    output = "You wish to know how much money is in account " + luisObj.topScoringIntent.actions[0].parameters[0].value[0].entity + "...";
+
+                } else if (intent == "MakePayment") {
+
+                    //this will make the payment from one to the other
+                    output = "You wish to make a payment of " + luisObj.topScoringIntent.actions[0].parameters[0].value[0].entity + " from your " + luisObj.topScoringIntent.actions[0].parameters[1].value[0].entity + " account to ";
+                    if (luisObj.topScoringIntent.actions[0].parameters[2].value[0] != null) {
+                        output += luisObj.topScoringIntent.actions[0].parameters[2].value[0].entity;
+                    } else {
+                        output += luisObj.topScoringIntent.actions[0].parameters[3].value[0].entity;
+                    }
+
+                }
+
+                Activity reply = activity.CreateReply($"" + output);
                 await connector.Conversations.ReplyToActivityAsync(reply);
 
             }
