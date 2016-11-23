cac7a2b700f5663c503ec09ff9dddf40415e71f9
TOMDESKTOP\Andrew <andrew-lyall@hotmail.com>
2016-11-24 12:08:30 +1300

    state services implemented

--- BankBot/Controllers/MessagesController.cs
+++ BankBot/Controllers/MessagesController.cs
@@ -41,20 +41,41 @@ namespace BankBot
 
                 string intent = luisObj.topScoringIntent.intent;
 
+                if (intent == "SetName") {
+
+                    string newName = luisObj.topScoringIntent.actions[0].parameters[0].value[0].entity;
+                    userData.SetProperty<string>("Name", " " + newName.Substring(0,1).ToUpper() + newName.Substring(1));
+                    await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+                    await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Hello" + userData.GetProperty<string>("Name") + "!"));
+                    return Request.CreateResponse(HttpStatusCode.OK);
+
+                } else if (intent == "ClearData") {
+
+                    await stateClient.BotState.DeleteStateForUserAsync(activity.ChannelId, activity.From.Id);
+                    await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Your data has been cleared"));
+                    return Request.CreateResponse(HttpStatusCode.OK);
+
+                }
+
                 if (!userData.GetProperty<bool>("SendGreeting")) {
+
                     userData.SetProperty<bool>("SendGreeting", true);
                     await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+
                     if (intent == "Greeting" || intent == "None") {
-                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Hello! How can I help you?"));
+                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Hello" + userData.GetProperty<string>("Name") + "! How can I help you?"));
                         return Request.CreateResponse(HttpStatusCode.OK);
                     } else {
-                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Hello!"));
+                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Hello" + userData.GetProperty<string>("Name") + "!"));
                     }
+
                 } else {
+
                     if (intent == "Greeting") {
-                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Hello Again!"));
+                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Hello Again" + userData.GetProperty<string>("Name") + "!"));
                         return Request.CreateResponse(HttpStatusCode.OK);
                     }
+
                 }
 
                 string output = "";
@@ -77,18 +98,26 @@ namespace BankBot
 
                 } else if (intent == "MakePayment") {
 
-                    //this will make the payment from one to the other
-                    output = "You wish to make a payment of " + luisObj.topScoringIntent.actions[0].parameters[0].value[0].entity + " from your " + luisObj.topScoringIntent.actions[0].parameters[1].value[0].entity + " account to ";
-                    if (luisObj.topScoringIntent.actions[0].parameters[2].value[0] != null) {
-                        output += luisObj.topScoringIntent.actions[0].parameters[2].value[0].entity;
-                    } else {
-                        output += luisObj.topScoringIntent.actions[0].parameters[3].value[0].entity;
+                    string amount = luisObj.topScoringIntent.actions[0].parameters[3].value[0].entity;
+                    string account = luisObj.topScoringIntent.actions[0].parameters[0].value[0].entity;
+                    string payee = "undefined";
+                    if (luisObj.topScoringIntent.actions[0].parameters[1].value[0] != null) {
+                        payee = luisObj.topScoringIntent.actions[0].parameters[1].value[0].entity;
                     }
+                    else if (luisObj.topScoringIntent.actions[0].parameters[2].value[0] != null) {
+                        payee = luisObj.topScoringIntent.actions[0].parameters[2].value[0].entity;
+                    }
+
+                    //this will make the payment from one to the other
+                    output = "You wish to make a payment of " + amount + " from your " + account + " account to " + payee;
+                    
 
                 }
 
-                Activity reply = activity.CreateReply($"" + output);
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                if (output != "") {
+                    Activity reply = activity.CreateReply($"" + output);
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
 
             }
             else {
