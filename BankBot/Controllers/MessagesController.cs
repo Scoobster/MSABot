f4205d531baac20c0fb35c1d676932d6df35382c
TOMDESKTOP\Andrew <andrew-lyall@hotmail.com>
2016-11-24 16:10:50 +1300

    Added UI cards and a non-functional cognitive services (but doesn't break bot)

--- BankBot/Controllers/MessagesController.cs
+++ BankBot/Controllers/MessagesController.cs
@@ -9,6 +9,8 @@ using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
 using BankBot.Models;
 using System.Collections.Generic;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
 
 namespace BankBot
 {
@@ -24,6 +26,8 @@ namespace BankBot
         private readonly string BANK_ADDRESS = "12 Grafton Road, Auckland";
         private readonly string BANK_LOGO = "https://s12.postimg.org/8pshbpbyl/Contoso_Bank_Logo.jpg";
         private readonly string BANK_MAP = "https://s16.postimg.org/pmxx1efed/Bank_Map.png";
+        private readonly string CROSS_IMAGE = "https://s18.postimg.org/baqrd9j55/Cross.png";
+        private readonly string TICK_IMAGE = "https://s14.postimg.org/v2q7rcstd/Tick.png";
 
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity) {
             if (activity.Type == ActivityTypes.Message) {
@@ -42,51 +46,59 @@ namespace BankBot
 
                 LUISObject.RootObject luisObj;
 
+                if (activity.Attachments.Count > 0) {
+                    VisionServiceClient VisionServiceClient = new VisionServiceClient("fff4c0ce2c8946a1a4a6bd8b951d13c6");
+                    AnalysisResult analysisResult = await VisionServiceClient.DescribeAsync(activity.Attachments[0].ContentUrl, 3);
+                    activity.Text = analysisResult.Description.Captions[0].Text;
+                }
+
                 HttpClient client = new HttpClient();
                 string x = await client.GetStringAsync(new Uri("https://api.projectoxford.ai/luis/v2.0/apps/a4ab66bf-68bb-4e95-8951-8b179e6f3c21?subscription-key=14b2e4303e5948578989704f3ef1fd87&q=" + activity.Text + "&verbose=true"));
                 luisObj = JsonConvert.DeserializeObject<LUISObject.RootObject>(x);
 
                 string intent = luisObj.topScoringIntent.intent;
 
-                if (activity.Text.Substring(0,8).ToLower() == "location") {
+                if (activity.Text.Length > 9) {
+                    if (activity.Text.Substring(0, 8).ToLower() == "location") {
 
-                    string location = activity.Text.Substring(9);
+                        string location = activity.Text.Substring(9);
 
-                    DirectionsObject.RootObject dirObj;
-                    string y = await client.GetStringAsync(new Uri("https://maps.googleapis.com/maps/api/directions/json?origin=" + location + "&destination=" + BANK_ADDRESS + "&key=AIzaSyBUytVVv7rrAWHM45JPuRgj52OEV_LwBnE"));
-                    dirObj = JsonConvert.DeserializeObject<DirectionsObject.RootObject>(y);
+                        DirectionsObject.RootObject dirObj;
+                        string y = await client.GetStringAsync(new Uri("https://maps.googleapis.com/maps/api/directions/json?origin=" + location + "&destination=" + BANK_ADDRESS + "&key=AIzaSyBUytVVv7rrAWHM45JPuRgj52OEV_LwBnE"));
+                        dirObj = JsonConvert.DeserializeObject<DirectionsObject.RootObject>(y);
 
-                    string duration = dirObj.routes[0].legs[0].duration.text;
-                    string distance = dirObj.routes[0].legs[0].distance.text;
-                    string bankLocation = dirObj.routes[0].legs[0].end_address;
+                        string duration = dirObj.routes[0].legs[0].duration.text;
+                        string distance = dirObj.routes[0].legs[0].distance.text;
+                        string bankLocation = dirObj.routes[0].legs[0].end_address;
 
-                    Activity locateReply = activity.CreateReply($"The distance to the closest bank is " + distance);
-                    locateReply.Recipient = activity.From;
-                    locateReply.Type = "message";
-                    locateReply.Attachments = new List<Attachment>();
+                        Activity locateReply = activity.CreateReply($"The distance to the closest bank is " + distance);
+                        locateReply.Recipient = activity.From;
+                        locateReply.Type = "message";
+                        locateReply.Attachments = new List<Attachment>();
 
-                    List<CardImage> cardImg = new List<CardImage>();
-                    cardImg.Add(new CardImage(url: BANK_MAP));
+                        List<CardImage> cardImg = new List<CardImage>();
+                        cardImg.Add(new CardImage(url: BANK_MAP));
 
-                    List<CardAction> cardAct = new List<CardAction>();
-                    cardAct.Add(new CardAction() {
-                        Value = "https://www.google.co.nz/maps/dir/" + location + "/" + BANK_ADDRESS,
-                        Type = "openUrl",
-                        Title = "Open Google Maps"
-                    });
+                        List<CardAction> cardAct = new List<CardAction>();
+                        cardAct.Add(new CardAction() {
+                            Value = "https://www.google.co.nz/maps/dir/" + location + "/" + BANK_ADDRESS,
+                            Type = "openUrl",
+                            Title = "Open Google Maps"
+                        });
 
-                    ThumbnailCard plCard = new ThumbnailCard() {
-                        Title = "Closest Bank",
-                        Subtitle = "The closest bank is at " + BANK_ADDRESS + " and is " + duration + " away.",
-                        Images = cardImg,
-                        Buttons = cardAct
-                    };
+                        HeroCard plCard = new HeroCard() {
+                            Title = "Closest Bank",
+                            Subtitle = "The closest bank is at " + BANK_ADDRESS + " and is " + duration + " away.",
+                            Images = cardImg,
+                            Buttons = cardAct
+                        };
 
-                    locateReply.Attachments.Add(plCard.ToAttachment());
-                    await connector.Conversations.SendToConversationAsync(locateReply);
-                    
-                    return Request.CreateResponse(HttpStatusCode.OK);
+                        locateReply.Attachments.Add(plCard.ToAttachment());
+                        await connector.Conversations.SendToConversationAsync(locateReply);
 
+                        return Request.CreateResponse(HttpStatusCode.OK);
+
+                    }
                 }
 
                 if (intent == "SetName") {
@@ -130,8 +142,7 @@ namespace BankBot
 
                 if (intent == "None") {
 
-                    await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Sorry I didn't quite understand what you meant."));
-                    output = "Could you please phrase it differently?";
+                    output = "Sorry I didn't quite understand what you meant. Could you please phrase it differently?";
 
                 } else if (intent == "FindBank") {
 
@@ -139,8 +150,14 @@ namespace BankBot
 
                 } else if (intent == "CheckBalance") {
 
-                    //Use the database to return amount of money in said bank account
-                    output = "You wish to know how much money is in account " + luisObj.topScoringIntent.actions[0].parameters[0].value[0].entity + "...";
+                    string account = "spending";
+                    if (luisObj.topScoringIntent.actions[0].parameters[0].value != null) {
+                        account = luisObj.topScoringIntent.actions[0].parameters[0].value[0].entity;
+                    }
+
+                    double amount = 0; //Look in database
+
+                    output = "You have " + amount + " in your " + account + " account";
 
                 } else if (intent == "MakePayment") {
 
@@ -154,8 +171,67 @@ namespace BankBot
                         payee = luisObj.topScoringIntent.actions[0].parameters[2].value[0].entity;
                     }
 
-                    //this will make the payment from one to the other
-                    output = "You wish to make a payment of " + amount + " from your " + account + " account to " + payee;
+                    Activity replyWeb = activity.CreateReply($"You wish to make a payment of " + amount + " from your " + account + " account to " + payee);
+                    replyWeb.Recipient = activity.From;
+                    replyWeb.Type = "message";
+                    replyWeb.Attachments = new List<Attachment>();
+
+                    List<CardImage> cardImg = new List<CardImage>();
+                    cardImg.Add(new CardImage(url: BANK_LOGO));
+
+                    // ADD LOGIC TO BUTTONS
+                    List<CardAction> cardAct = new List<CardAction>();
+                    cardAct.Add(new CardAction() {
+                        Value = "http://msa.ms",
+                        Type = "openUrl",
+                        Title = "Confirm",
+                        Image = TICK_IMAGE
+                    });
+                    cardAct.Add(new CardAction() {
+                        Value = "http://msa.ms",
+                        Type = "openUrl",
+                        Title = "Cancel",
+                        Image = CROSS_IMAGE
+                    });
+
+                    HeroCard plCard = new HeroCard() {
+                        Title = "Confirm Payment",
+                        Subtitle = "Please confirm that you wish to make this payment",
+                        Images = cardImg,
+                        Buttons = cardAct
+                    };
+
+                    replyWeb.Attachments.Add(plCard.ToAttachment());
+                    await connector.Conversations.ReplyToActivityAsync(replyWeb);
+                    return Request.CreateResponse(HttpStatusCode.OK);
+
+                } else if (intent == "Website") {
+
+                    Activity replyWeb = activity.CreateReply($"Here is our website");
+                    replyWeb.Recipient = activity.From;
+                    replyWeb.Type = "message";
+                    replyWeb.Attachments = new List<Attachment>();
+
+                    List<CardImage> cardImg = new List<CardImage>();
+                    cardImg.Add(new CardImage(url: BANK_LOGO));
+
+                    List<CardAction> cardAct = new List<CardAction>();
+                    cardAct.Add(new CardAction() {
+                        Value = "http://msa.ms",
+                        Type = "openUrl",
+                        Title = "Open Website"
+                    });
+
+                    HeroCard plCard = new HeroCard() {
+                        Title = "Contoso Website",
+                        Subtitle = "Take a look at our website for even more options.",
+                        Images = cardImg,
+                        Buttons = cardAct
+                    };
+
+                    replyWeb.Attachments.Add(plCard.ToAttachment());
+                    await connector.Conversations.ReplyToActivityAsync(replyWeb);
+                    return Request.CreateResponse(HttpStatusCode.OK);
 
                 }
 
@@ -163,6 +239,8 @@ namespace BankBot
                     Activity reply = activity.CreateReply($"" + output);
                     await connector.Conversations.ReplyToActivityAsync(reply);
                 }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
 
             }
             else {
